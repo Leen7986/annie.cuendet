ca9dc74106a3eeb972eb961be436f79e7531afdb
Leen7986 <einna_7986@icloud.com>
2022-04-04 10:43:23 +0200

    04.04.2022

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html> <!-- indicates that the site is in HTML -->
+<html lang="fr">
+
+	<head>
+        <link rel="stylesheet" href="style.css" />
+		<title>CV Annie Cuendet</title> <!-- page or tab title -->
+	</head>
+
+	<body>
+		
+	</body>
+</html>
